fb601b1026cdeed6b61da44ba7cfc84fdf108309
Matthew Meadows <Matthewmeadows4@gmail.com>
2023-03-25 11:24:51 -0400

    Moved classe to seperate files

---
+++ Combat.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace MyApp;
+
+public class Combat
+{
+    public Combat()
+    {
+
+        var Random = new Random();
+        List<string> mons = new List<string>();
+
+        mons.Add("Bulbasaur");
+        mons.Add("Charizard");
+        mons.Add("Squirtle");
+        mons.Add("Pikachu");
+        mons.Add("MewTwo");
+        mons.Add("Eevee");
+        mons.Add("Mew");
+        mons.Add("Greninja");
+        mons.Add("Snorlax");
+
+
+        int index = Random.Next(mons.Count);
+        string oMon = mons[index];
+        int playerhealth = 20;
+        int enemyhealth = 20;
+
+        while (playerhealth > -1 && enemyhealth > -1)
+        {
+            Console.WriteLine("The " + " has " + playerhealth + " health");
+            Console.WriteLine("The " + oMon + " has" + enemyhealth + " health");
+            Console.WriteLine("press 1 to attack, or 2 to heal");
+            string choice = Console.ReadLine();
+            int enemydamage = new Random().Next(1, 4);
+
+            if (choice == "1")
+            {
+                Console.WriteLine("You attacked for 2 damage");
+                enemyhealth -= 2;
+                Console.WriteLine("The " + oMon + " attacked for " + enemydamage + " Damage!");
+                playerhealth -= enemydamage;
+            }
+            if (choice == "2")
+            {
+                Console.WriteLine("You healed for " + enemydamage + "Health");
+                Console.WriteLine("The" + oMon + " wouldve dealt " + enemydamage + " damage.");
+                playerhealth += enemydamage;
+            }
+        }
+    }
+}
+	
