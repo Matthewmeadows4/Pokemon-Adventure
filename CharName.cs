011c8a42b92ed020c925800a6e8037256f96ba49
Matthew Meadows <Matthewmeadows4@gmail.com>
2023-03-30 18:49:49 -0400

    Fixed errors in CharName class

--- CharName.cs
+++ CharName.cs
@@ -8,14 +8,16 @@ public class CharName
     public static string name()
     {
         Charizard charizard = new Charizard();
-        Console.WriteLine("I see that Professor Oak has given you a fully evolved Charizard! A" + charizard.Type + "asdasd");
+        Console.WriteLine("I see that Professor Oak has given you a fully evolved Charizard, a " + charizard.Type + " Pokemon!");
 
         Console.WriteLine("What is your new Charizard's Name?");
         
         string name = Console.ReadLine();
         charizard.Name = name;
         Console.WriteLine(charizard.Name + "?... Excellent Choice!");
+
         return charizard.Name;
+      
 
     }
 }
